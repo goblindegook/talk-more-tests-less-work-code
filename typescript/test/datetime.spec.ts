bc7a4f6c8f11cefd55097532c3486dd073eae646
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-04-10 00:33:33 +0100

    Better datetime example

--- typescript/test/datetime.spec.ts
+++ typescript/test/datetime.spec.ts
@@ -1,11 +1,23 @@
 import { property, nat, bless } from 'jsverify'
 
+xdescribe('Arbitrary datetime with bless', () => {
+  const datetime = bless({
+    generator: nat(Number.MAX_SAFE_INTEGER).generator.map(n => new Date(n * 3600))
+    // No shrinker!
+  })
+
+  property('generates dates', datetime, d => d instanceof Date)
+
+  // This will fail, but notice how the unshrunk value is less helpful:
+  property('generates past dates', datetime, d => d < new Date())
+})
+
 xdescribe('Arbitrary datetime with smap', () => {
   const datetime = nat(Number.MAX_SAFE_INTEGER).smap(
     // generator
-    n => new Date(n),
+    n => new Date(n * 3600),
     // shrink
-    d => d.getTime(),
+    d => d.getTime() / 3600,
     // show
     d => d.toString()
   )
@@ -13,17 +25,5 @@ xdescribe('Arbitrary datetime with smap', () => {
   property('generates dates', datetime, d => d instanceof Date)
 
   // This will fail:
-  property('generates future dates', datetime, d => d > new Date())
-})
-
-xdescribe('Arbitrary datetime with bless', () => {
-  const datetime = bless({
-    generator: nat(Number.MAX_SAFE_INTEGER).generator.map(n => new Date(n))
-    // No shrinker!
-  })
-
-  property('generates dates', datetime, d => d instanceof Date)
-
-  // This will fail, but notice how the unshrunk value is less helpful:
-  property('generates future dates', datetime, d => d > new Date())
+  property('generates past dates', datetime, d => d < new Date())
 })
